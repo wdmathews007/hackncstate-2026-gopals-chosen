eec73f824c4224d0dcbfe42f254c0edabc31bf01
NathanielR2 <ramirezna22@students.ecu.edu>
2026-02-14 23:14:20 -0500

    bugFIxes but not all

--- backend/upload/router.py
+++ backend/upload/router.py
@@ -1,11 +1,11 @@
-from fastapi import APIRouter, UploadFile, File, HTTPException
-from stdlib import uuid, os
-from pthlib import Path
+from fastapi import APIRouter, UploadFile, File, HTTPException, FastAPI
+import uuid, os
+from pathlib import Path
 
 app = FastAPI()
 router = APIRouter()
 
-UPLOAD_DIR = Path(__file__).parent.parent+"/upload/pictures"
+UPLOAD_DIR = Path(str(Path(__file__).parent.parent)+"/upload/pictures")
 os.mkdir(UPLOAD_DIR,parents=True,exist_ok=True)
 
 @app.post("/upload/")
@@ -18,7 +18,7 @@ async def upload_file(UploadFile: UploadFile):
     
     file = open(UploadFile.filename, "r")
     filename= f"{uuid.uuid4().hex}_{file.filename}"
-    with open(UPLOAD_DIR + filename, "w") as f:
+    with open(Path(str(UPLOAD_DIR) + filename), "w") as f:
         f.write(content)
     content = await file.read()
     return {"filename":filename,"status":"uploaded","analysis":{"label":"unknown","confidence":0.0,"metadata": {},"heatmap_url":None}}
