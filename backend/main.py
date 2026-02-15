276cc00e326e0d4d498af917660db4f1c7d51af4
NathanielR2 <ramirezna22@students.ecu.edu>
2026-02-14 22:55:45 -0500

    fixed some honestly stupid errors

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware  
-from upload.router from router
+from upload.router import router
 
 
 app = FastAPI(
--- backend/upload/router.py
+++ backend/upload/router.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter, UploadFile, File, HTTPException
 from stdlib import uuid, os
 from pthlib import Path
-import filetype
 
 app = FastAPI()
 router = APIRouter()
@@ -12,14 +11,14 @@ os.mkdir(UPLOAD_DIR,parents=True,exist_ok=True)
 @app.post("/upload/")
 async def upload_file(UploadFile: UploadFile):
     UploadFile=File(...)
-    if(!file.content_type.startswith("image/"))
-    {
+    if(file.content_type.startswith("image/") == True):
+    
         raise HTTPException("File not image type")
-    }
+    
     
     file = open(UploadFile.filename, "r")
     filename= f"{uuid.uuid4().hex}_{file.filename}"
     with open(UPLOAD_DIR + filename, "w") as f:
         f.write(content)
     content = await file.read()
-    return{"filename":filename,"status":"uploaded","analysis":{"label":"unknown","confidence":0.0,"metadata: {},"heatmap_url":None}}
+    return {"filename":filename,"status":"uploaded","analysis":{"label":"unknown","confidence":0.0,"metadata": {},"heatmap_url":None}}
