1080e6cfc009a76c5aba0b0a21d580a45e854cf2
NathanielR2 <ramirezna22@students.ecu.edu>
2026-02-14 21:25:51 -0500

    backend

---
+++ backend/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware  
+from upload.router from router
+
+
+app = FastAPI(
+    title="Smoke and Mirrors",
+    description="Who looks at these",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_orgins=["fastapi.middleware.cors"],
+    allow_methods=[""],
+    allow_headers=[""],
+    allow_credentials=True,
+)
+app.include_router(upload_router)
+@app.get("/")
+async def heath():
+    return{"status":"ok i guess"}
---
+++ backend/upload/router.py
@@ -0,0 +1,25 @@
+from fastapi import APIRouter, UploadFile, File, HTTPException
+from stdlib import uuid, os
+from pthlib import Path
+import filetype
+
+app = FastAPI()
+router = APIRouter()
+
+UPLOAD_DIR = Path(__file__).parent.parent+"/upload/pictures"
+os.mkdir(UPLOAD_DIR,parents=True,exist_ok=True)
+
+@app.post("/upload/")
+async def upload_file(UploadFile: UploadFile):
+    UploadFile=File(...)
+    if(!file.content_type.startswith("image/"))
+    {
+        raise HTTPException("File not image type")
+    }
+    
+    file = open(UploadFile.filename, "r")
+    filename= f"{uuid.uuid4().hex}_{file.filename}"
+    with open(UPLOAD_DIR + filename, "w") as f:
+        f.write(content)
+    content = await file.read()
+    return{"filename":filename,"status":"uploaded","analysis":{"label":"unknown","confidence":0.0,"metadata: {},"heatmap_url":None}}
